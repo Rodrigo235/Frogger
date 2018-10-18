928e835813c0bd44dddf72b9cb36e02c454b7873
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-10-18 09:25:29 -0300

    bla

--- objects/globals.lua
+++ objects/globals.lua
@@ -2,8 +2,6 @@ largura, altura = display.actualContentWidth, display.actualContentHeight
 
 centroX, centroY = display.contentCenterX, display.contentCenterY
 
-
-
 tamanhoPersonagem = largura * 0.1
 
 larguraSegura, alturaSegura = display.safeActualContentWidth, display.safeActualContentHeight
